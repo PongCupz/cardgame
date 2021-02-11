ca712aded16bd1655e33e5a4f3cf4ca483e1436c
pongcupz <ppl123_pong@hotmail.com>
2021-02-12 03:49:42 +0700

    change config prod

--- my-app/config/prod.env.js
+++ my-app/config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"http://192.168.1.104:8080"',
-  ROOT_WS: '"ws://192.168.1.104:15675/ws"',
+  ROOT_API: '"http://localhost:8080"',
+  ROOT_WS: '"ws://localhost:15675/ws"',
   GG_KEY: '"AIzaSyDdEEPcJpCni349iXn4W3z6HIC5N480Q0k"'
 }
